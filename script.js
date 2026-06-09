8181f970e0643e6ca575fcf8ccbf017c0af82d29
certefied-dumbass <24399lc@hvhs.school.nz>
2026-06-09 12:27:46 +1200

    starting to hate js but y'know, i have to do it for school, but i rlly dont want to even tho its important, why doesnt the order form work anymore, its rlly stoopid and stuff any ways how was your day reader, im gonna go on a really long tangent for the bit, but i reallly REALLY hat javascript becasue its to hard to learn and it whines about anything

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello world!")
+
+let shoppingList = [];
+
+function getFormInputShopping(){
+    const SHOPPING_FIELD = document.getElementById("shoppingField")
+    let item = SHOPPING_FIELD.value
+    shoppingList.push(item)
+    OUTPUT.innerHTML = "<p>You have added "+item+" to your list</p>"
+    }
+    function displayList(){
+      OUTPUT.innerHTML = "<p>Your list:</p>"
+      for (i=0;i<shoppingList.length;i++){
+        OUTPUT.innerHTML += "<p>"+shoppingList[i]+"</p>"
+      }
+    }
+    OUTPUT.innerHTML += ("<p> Shopping list:"+shoppingList+"</p>")
+const SHOPPING_FIELD = document.getElementById("shoppingField")
+shoppingList.push(SHOPPING_FIELD.value)
+    OUTPUT.innerHTML += ("<p>Shopping list: "+shoppingList+"</p>")
